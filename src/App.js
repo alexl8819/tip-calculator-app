7d41a412a57aeca884d78814b40ab6e8d71ea086
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-04-14 19:01:48 -0700

    update eslint and app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useState, useMemo } from 'react';
 import styled from 'styled-components';
 
-import TwoColumnNumberInput from './components/Input';
+import TippingMenu from './components/Tip';
+import { TwoColumnNumberInput } from './components/Input';
 
 import { calculateOwed } from './util';
 
@@ -36,38 +37,6 @@ const CalculatorForm = styled.form`
   }
 `;
 
-const TippingFieldset = styled.fieldset`
-  display: flex;
-  flex-direction: column;
-  border: none;
-  padding: 0;
-  margin: 0;
-`;
-
-const TippingLegend = styled.legend`
-  color: hsl(183, 100%, 15%);
-  margin-bottom: 10px;  
-`;
-
-const TippingOptionLabel = styled.label`
-  width: 48%;
-  padding: 10px;
-  border-radius: 8px;
-  text-align: center;
-  color: hsl(0, 0%, 100%);
-  background-color: hsl(183, 100%, 15%);
-  cursor: pointer;
-
-  input[type="radio"]:checked + & {
-    background-color: hsl(172, 67%, 45%);
-  }
-`;
-
-const TippingOption = styled.input`
-  appearance: none;
-  display: none;
-`;
-
 const Col = styled.div`
   width: 100%;
 
@@ -76,25 +45,19 @@ const Col = styled.div`
   }
 `;
 
-const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin-bottom: 10px;
-`;
-
 const ResultPanel = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   background-color: hsl(183, 100%, 15%);
   border-radius: 8px;
   margin-top: 10px;
   padding: 25px;
+  height: 100%;
 
   @media screen and (min-width: 1024px) {
     margin-top: 0;
     min-width: 20rem;
-    height: 100%;
   }
 `;
 
@@ -162,60 +125,36 @@ export default function App () {
       <AppHeader>
         <img src={logo} alt="app logo" />
       </AppHeader>
-      <CalculatorForm name="bill-calculator" onSubmit={(e) => preventDefault()}>
+      <CalculatorForm name="bill-calculator" onSubmit={(e) => e.preventDefault()}>
         <Col>
-          <TwoColumnNumberInput icon="dollar" name="bill" label="Bill" alt="money icon" value={billAmount} placeholder="0" onInput={(e) => setBillAmount(e.target.value)} />
+          <TwoColumnNumberInput icon="dollar" name="bill" label="Bill" alt="money icon" value={billAmount} placeholder="0" min="1" onInput={(e) => setBillAmount(e.target.value)} required />
 
-          <TippingFieldset>
-            <TippingLegend>Select Tip %</TippingLegend>
-            
-            <Row>
-              <TippingOption type="radio" id="five-percent" name="tip" value="5" onChange={(e) => setTipPercentage(5)} />
-              <TippingOptionLabel htmlFor="five-percent">5%</TippingOptionLabel>
-
-              <TippingOption type="radio" id="ten-percent" name="tip" value="10" onChange={(e) => setTipPercentage(10)} />
-              <TippingOptionLabel htmlFor="ten-percent">10%</TippingOptionLabel>
-            </Row>
-            
-            <Row>
-              <TippingOption type="radio" id="fifteen-percent" name="tip" value="15" onChange={(e) => setTipPercentage(15)} />
-              <TippingOptionLabel htmlFor="fifteen-percent">15%</TippingOptionLabel>
-
-              <TippingOption type="radio" id="twentyfive-percent" name="tip" value="25" onChange={(e) => setTipPercentage(25)} />
-              <TippingOptionLabel htmlFor="twentyfive-percent">25%</TippingOptionLabel>
-            </Row>
-            
-            <Row>
-              <TippingOption type="radio" id="fifty-percent" name="tip" value="50" onChange={(e) => setTipPercentage(50)} />
-              <TippingOptionLabel htmlFor="fifty-percent">50%</TippingOptionLabel>
-              
-              <input type="number" placeholder="custom" onInput={(e) => setTipPercentage(e.target.value)} />
-              <label htmlFor="custom"></label>
-            </Row>
-          </TippingFieldset>
+          <TippingMenu />
           
-          <TwoColumnNumberInput icon="person" name="people" label="Number of People" alt="people icon" value={numOfPpl} placeholder="0" onInput={(e) => setNumOfPpl(e.target.value)} />
+          <TwoColumnNumberInput icon="person" name="people" label="Number of People" alt="people icon" value={numOfPpl} placeholder="0" min="1" onInput={(e) => setNumOfPpl(e.target.value)} required />
         </Col>
         <Col>
           <ResultPanel>
-            <PanelContainer>
-              <div>
-                <TitleDescription>Tip Amount</TitleDescription>
-                <PerPersonDescription>/ person</PerPersonDescription>
-              </div>
-              <TipAmount>${ calculateTipOwed }</TipAmount>
-            </PanelContainer>
-            <PanelContainer>
-              <div>
-                <TitleDescription>Total</TitleDescription>
-                <PerPersonDescription>/ person</PerPersonDescription>
-              </div>
-              <TotalAmount>${ calculateTotalOwed }</TotalAmount>
-            </PanelContainer>
+            <div>
+              <PanelContainer>
+                <div>
+                  <TitleDescription>Tip Amount</TitleDescription>
+                  <PerPersonDescription>/ person</PerPersonDescription>
+                </div>
+                <TipAmount>${ calculateTipOwed }</TipAmount>
+              </PanelContainer>
+              <PanelContainer>
+                <div>
+                  <TitleDescription>Total</TitleDescription>
+                  <PerPersonDescription>/ person</PerPersonDescription>
+                </div>
+                <TotalAmount>${ calculateTotalOwed }</TotalAmount>
+              </PanelContainer>
+            </div>
             <ResetButton type="reset" onClick={(e) => resetAll()}>Reset</ResetButton>
           </ResultPanel>
         </Col>
       </CalculatorForm>
     </Container>
   );
-};
+}
