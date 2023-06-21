e63017d219a06885424ad23a287a8cc17c605428
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-06-20 20:48:00 -0700

    refactor: configure parcel to use babel only for testing, add initial test script for Input component

---
+++ src/components/Input.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { CustomNumberInput } from './Input';
+
+const noop = () => {}
+
+describe('CustomNumberInput component', () => {
+  render(<CustomNumberInput name="test" placeholder="Custom" min="1" step="1" onInput={noop} />);
+
+  test('Should have rendered', () => {
+    expect(screen.getByPlaceholderText('Custom')).toBeTruthy();
+  });
+});
